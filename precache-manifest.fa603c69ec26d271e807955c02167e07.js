eadc738355ed642840ba845868970950990e3abb
TravisWongX <travis.x.wong@gmail.com>
2022-03-06 21:12:56 +0800

    deploy

---
+++ precache-manifest.fa603c69ec26d271e807955c02167e07.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85b3228099c84740a644",
+    "url": "/dobby-vue/css/app.4bce0ff6.css"
+  },
+  {
+    "revision": "0aa4944508946db60adc",
+    "url": "/dobby-vue/css/chunk-vendors.73418633.css"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "/dobby-vue/fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "/dobby-vue/fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "d5a8cd4c413b21884f76a242232121c4",
+    "url": "/dobby-vue/index.html"
+  },
+  {
+    "revision": "85b3228099c84740a644",
+    "url": "/dobby-vue/js/app.3b2576b2.js"
+  },
+  {
+    "revision": "0aa4944508946db60adc",
+    "url": "/dobby-vue/js/chunk-vendors.7e554088.js"
+  },
+  {
+    "revision": "3150727a70a9e016a1ec83088e86ce7f",
+    "url": "/dobby-vue/manifest.json"
+  },
+  {
+    "revision": "b664dbbbfc6cff5fd117f715403618a8",
+    "url": "/dobby-vue/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/dobby-vue/robots.txt"
+  }
+]);
